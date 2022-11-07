b112bd3e28dc31aa4197e3ce0eb6c11bde33e388
victoryju <kob0805@naver.com>
2022-11-07 09:24:54 +0900

    fork

--- src/components/RecipeComponent.tsx
+++ src/components/RecipeComponent.tsx
@@ -13,7 +13,6 @@ export const RecipeComponent = () => {
   const handleCopyRecipe = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
-
       alert('영수증 번호가 복사되었습니다.');
     } catch (e) {
       alert('복사 실패');
